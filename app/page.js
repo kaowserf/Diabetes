b4c686fb0ce73032c9112c7be69edd79b798d77f
kaowser Ahmed <kaowserahmedinfo@gmail.com>
2025-05-05 21:50:07 +0600

    Update second YouTube video link

--- app/page.js
+++ app/page.js
@@ -743,7 +743,7 @@ export default function Home() {
               <div className="relative pb-[56.25%] bg-gray-200 rounded-lg overflow-hidden">
                 <iframe 
                   className="absolute top-0 left-0 w-full h-full"
-                  src="https://www.youtube.com/embed/gBxFaQvT6ss" 
+                  src="https://www.youtube.com/embed/9iKNehXKwZM" 
                   title={t.video2Title}
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                   allowFullScreen
